aac93c51f5beceaa57cc60de9ae4958a4552f27a
Brenocunha47 <132768810+Brenocunha47@users.noreply.github.com>
2025-06-09 16:48:33 -0300

    Update livro.py

--- livro.py
+++ livro.py
@@ -1,5 +1,5 @@
 class Livro:
-    def __init__(self, titulo, autor, ano, isbn, status="disponível", nome_locatario=""):
+    def __init__(self, titulo, autor, ano, isbn, status, nome_locatario=""):
         self.titulo = titulo
         self.autor = autor
         self.ano = ano
@@ -8,6 +8,7 @@ class Livro:
         self.nome_locatario = nome_locatario
 
     def __str__(self):
-            if self.status == "alugado":
-                return f"{self.titulo} - {self.autor} ({self.ano}) ISBN: {self.isbn} | Alugado por: {self.nome_locatario}"
-            return f"{self.titulo} - {self.autor} ({self.ano}) ISBN: {self.isbn} | Disponível"
+        if self.status == "alugado":
+            return f"{self.titulo} - {self.autor} ({self.ano}) | ISBN: {self.isbn} | {self.status} por {self.nome_locatario}"
+        else:
+            return f"{self.titulo} - {self.autor} ({self.ano}) | ISBN: {self.isbn} | {self.status}"
